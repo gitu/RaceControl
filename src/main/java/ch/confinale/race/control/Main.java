20ef11eb88b9502f38c19084ddb14368e8245841
Florian Schrag <florian@schrag.org>
2014-10-01 23:33:58 +0200

    basic race data upload

--- src/main/java/ch/confinale/race/control/Main.java
+++ src/main/java/ch/confinale/race/control/Main.java
@@ -36,20 +36,33 @@ public class Main {
 
     public static void start(Configuration config) throws PortInUseException, NoSuchPortException, UnsupportedCommOperationException, TooManyListenersException, IOException, InterruptedException {
 
+        String raceId = UUID.randomUUID().toString();
+        Firebase firebase = new Firebase("https://" + config.getFirebaseName() + ".firebaseio.com/");
+        firebase.child("lastRace").setValue(raceId);
+
+
         ControlUnit controlUnit = new ControlUnit(config.getComPort());
         controlUnit.initialize();
 
         System.out.println(new String(controlUnit.sendCommand("0")));
         Thread.sleep(500);
         controlUnit.sendCommand("=", (byte) '=');
+        Thread.sleep(500);
 
-
-        Firebase firebase = new Firebase("https://" + config.getFirebaseName() + ".firebaseio.com/");
 
         Map<Integer, List<NewTimeResult>> timeResults = new HashMap<>();
+        long fastestRound = Long.MAX_VALUE;
+        long[] fastestCarRound = new long[10];
         for (int i = 0; i <= 9; i++) {
             timeResults.put(i, new LinkedList<>());
+            fastestCarRound[i] = Long.MAX_VALUE;
         }
+
+        int count = 0;
+        while(controlUnit.sendCommand("?")[1]!=':' || count++<=100) {
+            Thread.sleep(30);
+        }
+
 
         TrackStatusResult lastTrackStatusResult = null;
         while (true) {
@@ -70,12 +83,36 @@ public class Main {
                 if (newTimeResults.size()>0) {
                     lastResult = newTimeResults.get(newTimeResults.size()-1);
                 }
+                firebase.child("race").child(raceId)
+                        .child("car").child(((Integer) newResult.getCarNr()).toString())
+                        .child("raw")
+                        .child("round").child(((Integer)(newTimeResults.size() - 1)).toString())
+                        .setValue(newResult);
                 if (!newResult.equals(lastResult)) {
                     newTimeResults.add(newResult);
                     System.out.println(newResult);
                     if (newTimeResults.size() >= 2) {
+                        long roundTime = newResult.getTime() - newTimeResults.get(newTimeResults.size() - 2).getTime();
                         System.out.println("Time for car: " + newResult.getCarNr() + ", lap: " + (newTimeResults.size() - 1) + " - "
-                                + ((double) (newResult.getTime() - newTimeResults.get(newTimeResults.size() - 2).getTime())) / 1000.0 + "s");
+                                + ((double) roundTime) / 1000.0 + "s");
+
+                        firebase.child("race").child(raceId)
+                                .child("car").child(((Integer) newResult.getCarNr()).toString())
+                                .child("round").child(((Integer)(newTimeResults.size() - 1)).toString())
+                                .setValue(roundTime);
+
+                        if (fastestRound>roundTime) {
+                            fastestRound = roundTime;
+                            firebase.child("race").child(raceId)
+                                    .child("fastest")
+                                    .setValue(roundTime);
+                        }
+                        if (fastestCarRound[newResult.getCarNr()]>roundTime) {
+                            firebase.child("race").child(raceId)
+                                    .child("car").child(((Integer) newResult.getCarNr()).toString())
+                                    .child("fastest")
+                                    .setValue(roundTime);
+                        }
                     }
                 }
             }
